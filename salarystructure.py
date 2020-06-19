1b9adc68ee18a39b970b32ab4ca10c75591c17b5
Chukwume Ijeh <chukwumeijeh@gmail.com>
2020-06-19 23:38:46 +0100

    feat: Change output to just monthly PAYE

--- salarystructure.py
+++ salarystructure.py
@@ -1,37 +1,38 @@
-first_name = input('Enter first name: ').title()
-last_name = input('Enter last name: ').title()
+# Code to calculate the monthly PAYE of an employee
+
+first_name = input('Enter first name of employee: ').title()
+last_name = input('Enter last name of employee: ').title()
 
 while True:
     try:
-        gross_annual = int(input("What is the Gross Annual Salary of employee? \nDo not include any symbols. Eg. 'NGN2,500,500' should be written as '2500000'\n"))
+        gross_annual = int(input("What is the Gross Annual Income of employee? \nDo not include any symbols. Eg. 'NGN2,500,000' should be written as '2500000'\n"))
         break
     except:
-        print ('Enter a valid Gross Annual Salary.')
+        print ('\nEnter a valid Gross Annual Income.')
         continue
 
-basic = 0.39 * gross_annual # Assuming 39% of annual salary is 'Basic'
-housing = 0.29 * gross_annual # Assuming 29% of annual salary is 'Housing'
+basic = 0.4 * gross_annual # Assuming 40% of annual salary is 'Basic'
+housing = 0.3 * gross_annual # Assuming 30% of annual salary is 'Housing'
 transport = 0.1 * gross_annual # Assuming 10% of annual salary is 'Transport'
 
-relief = (gross_annual * 0.2) + 200000 # Consolidated Relief of N200,000 plus 20% of Gross annual income
-pension = 0.08 * (basic + housing + transport) # Assuming 8% pension
+relief = (gross_annual * 0.2) + 200000   # Consolidated Relief of N200,000 plus 20% of Gross Annual Income
+pension = 0.08 * (basic + housing + transport)   # Assuming 8% pension
 
 taxable_income = gross_annual - (relief + pension)
 
 if taxable_income <= 300000:
-    paye_annum = 0.07 * taxable_income
+    paye_annum = 0.07 * taxable_income   # 7% tax on first NGN300,000
 elif taxable_income <= 600000:
-    paye_annum = (0.07 * 300000) + (0.11*(taxable_income - 300000))
+    paye_annum = (0.07 * 300000) + (0.11*(taxable_income - 300000))  # 11% tax on next NGN300,000
 elif taxable_income <= 1100000:
-    paye_annum = (0.07 * 300000) + (0.11 * 300000) + (0.15*(taxable_income - 600000))
+    paye_annum = (0.07 * 300000) + (0.11 * 300000) + (0.15*(taxable_income - 600000))   # 15% tax on next NGN500,000
 elif taxable_income <= 1600000:
-    paye_annum = (0.07 * 300000) + (0.11 * 300000) + (0.15 * 500000) + (0.19*(taxable_income - 1100000))
+    paye_annum = (0.07 * 300000) + (0.11 * 300000) + (0.15 * 500000) + (0.19*(taxable_income - 1100000))    # 19% tax on next NGN500,000
 elif taxable_income <= 3200000:
-    paye_annum = (0.07 * 300000) + (0.11 * 300000) + (0.15 * 500000) + (0.19 * 500000) + (0.21*(taxable_income - 1600000))
+    paye_annum = (0.07 * 300000) + (0.11 * 300000) + (0.15 * 500000) + (0.19 * 500000) + (0.21*(taxable_income - 1600000))    # 21% tax on next NGN1,600,000
 else:
-    paye_annum = (0.07 * 300000) + (0.11 * 300000) + (0.15 * 500000) + (0.19 * 500000) + (0.21 * 1600000) + (0.24*(taxable_income - 3200000))
+    paye_annum = (0.07 * 300000) + (0.11 * 300000) + (0.15 * 500000) + (0.19 * 500000) + (0.21 * 1600000) + (0.24*(taxable_income - 3200000))    # 24% tax on remaining amount
 
-yearly_net = gross_annual - paye_annum - pension
-monthly_net = yearly_net / 12
+paye_monthly = paye_annum / 12    # monthly PAYE
 
-print('Monthly and yearly salary of {} {} is NGN{} and NGN{} respectively.'.format(first_name, last_name, round(monthly_net,2), round(yearly_net,2)))
+print('The monthly PAYE of {} {} is NGN{}.'.format(first_name, last_name, round(paye_monthly, 2)))
